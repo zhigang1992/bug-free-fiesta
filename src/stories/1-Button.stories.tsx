caeb6e28ea9f80ca8daf5ecc021fe7f5656e919b
Kyle Fang <zhigang1992@gmail.com>
2020-01-03 23:16:48 +0800

    feat: add test

---
+++ src/stories/1-Button.stories.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { Button } from '@storybook/react/demo';
+
+import './testFails.css';
+import styles from './test.module.css';
+
+// This works
+export const Text = () => <div className={styles.testClass}/>;
+// This does not work
+// export const Text = () => <div className="testClass"/>;
+
+export const Emoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      😀 😎 👍 💯
+    </span>
+  </Button>
+);
+
+export default {
+    title: 'Button',
+    component: Button,
+};
