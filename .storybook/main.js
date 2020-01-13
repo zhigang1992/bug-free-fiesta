ad6331cb7f428fed9d37e04026f11017efa8b859
Kyle Fang <zhigang1992@gmail.com>
2020-01-13 22:21:12 +0800

    fix: css import issue

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
   addons: [
     "@storybook/addon-actions",
     "@storybook/addon-links",
+    "@storybook/addon-knobs",
     {
       name: "@storybook/preset-create-react-app",
       options: {
--- src/stories/1-Button.stories.tsx
+++ src/stories/1-Button.stories.tsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
 
-import './testFails.css';
-import styles from './test.module.css';
+import '../testFails.css';
 
-// This works
-// export const Text = () => <div className={styles.testClass}/>;
-// This does not work
 export const Text = () => <div className="testClass"/>;
 
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
     <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
+      😀 😎
     </span>
   </Button>
 );
